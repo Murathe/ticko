8db1c9498bd65fad78bd9089836418f4cc8c0536
murathe <isaacsm07p@gmail.com>
2020-02-28 11:37:41 +0300

    [Not initial commit] bulk commit

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/moringa/Documents/moringa-school-projects/core/py/Django/ticko/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
